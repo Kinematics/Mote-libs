3f0b8e3e321f253e72114afaa40c37d71f214420
David Smith <dsmith@datasync.com>
2014-08-09 13:49:08 -0500

    Add comments

--- Modes.lua
+++ Modes.lua
@@ -200,6 +200,7 @@ end
 
 _meta.M.__methods = {}
 
+-- Cycle forwards through the list
 _meta.M.__methods['cycle'] = function(m)
 	if m._track._type == 'list' then
 		m._track._current = (m._track._current % m._track._count) + 1
@@ -210,6 +211,7 @@ _meta.M.__methods['cycle'] = function(m)
 	return m.Current
 end
 
+-- Cycle backwards through the list
 _meta.M.__methods['cycleback'] = function(m)
 	if m._track._type == 'list' then
 		m._track._current = m._track._current - 1
@@ -223,6 +225,7 @@ _meta.M.__methods['cycleback'] = function(m)
 	return m.Current
 end
 
+-- Toggle a boolean value
 _meta.M.__methods['toggle'] = function(m)
 	if m._track._type == 'boolean' then
 		m._track._current = not m._track._current
@@ -233,6 +236,7 @@ _meta.M.__methods['toggle'] = function(m)
 	return m.Current
 end
 
+-- Set the current value
 _meta.M.__methods['set'] = function(m, val)
 	if m._track._type == 'boolean' then
 		if type(val) == 'boolean' then
@@ -271,6 +275,7 @@ _meta.M.__methods['set'] = function(m, val)
 	return m.Current
 end
 
+-- Reset to the default value
 _meta.M.__methods['reset'] = function(m)
     m._track._current = m._track._default
 
